8e956b8ade3e5a16746a4069b4e38328fa0e4d5a
snsmith90 <sns@umai.ucsb.edu>
2012-05-30 22:13:13 -0700

    Looks like everything is working

--- checker.scala
+++ checker.scala
@@ -9,34 +9,23 @@ object Checker {
   def main(args:Array[String]) {
     // parse the given program
     val ast = ParseL.getAST( Source.fromFile( args(0) ).mkString )
-    
-    // either print the AST or typecheck it
-   /* if ( args.length > 1 && args(1) == "--ast" )
-      //printDot( ast )
-    else */
-      //println( (inScope( Env(), Public() ) eval ast).find )
-      println(ast)
 
-		// adding secret, alice, bob, and public to setOfSets
+    // adding secret, alice, bob, and public to setOfSets
+    setOfSets.addToBigSet(Secret)
+    setOfSets.addToBigSet(Alice)
+    setOfSets.addToBigSet(Bob)
+    setOfSets.addToBigSet(Public)
 
-
-		setOfSets.addToBigSet(Secret)
-		setOfSets.addToBigSet(Alice)
-		setOfSets.addToBigSet(Bob)
-		setOfSets.addToBigSet(Public)
-
-  	//System.exit(1)
+  	// levelcheck it
     evalLevel().eval(Env(),Public, ast)
 
-		println("bigSet: " +setOfSets.bigSet)
-		println("Checking Secret...")
-		BFS.algo(Secret)
-		println("Checking Alice...")
-		BFS.algo(Alice)
-		println("Checking Bob...")
-		BFS.algo(Bob)
-		println("Secure") 
-
+    //println("Checking Secret...")
+    BFS.algo(Secret)
+    //println("Checking Alice...")
+    BFS.algo(Alice)
+    //println("Checking Bob...")
+    BFS.algo(Bob)
+    println("Secure")
 		
   }
   
@@ -67,7 +56,6 @@ case class Env( env:Map[String, LVar] = Map() ) {
 case class evalLevel(){
       def eval( ρ:Env, l_w:Level, t:Term ): Level = t match {
         case Then( ts ) ⇒ {
-          // ts.map(eval).last              //TODO: for each loops through ts and calls eval returns level of last term
           var temp = l_w // temp holder
           ts.foreach(  i => temp = eval( ρ, l_w, i) )
           temp
@@ -75,32 +63,30 @@ case class evalLevel(){
 
         case Assign( Var(x), e ) ⇒
         {
-           var l1 = eval( ρ, l_w, e )
-           var l2 =  ρ( x )
-          println("Assign " + l1 + " ⊑ " + l2)
-					setOfSets.addToListBuffer(l1, l2)
-          println("Assign " + l_w + " ⊑ " + l2)
-					setOfSets.addToListBuffer(l_w, l2)
-          l_w
+            var l1 = eval( ρ, l_w, e )
+            var l2 =  ρ( x )
+            //println("Assign " + l1 + " ⊑ " + l2)
+            setOfSets.addToListBuffer(l1, l2)
+            //println("Assign " + l_w + " ⊑ " + l2)
+            setOfSets.addToListBuffer(l_w, l2)
+            l_w
         }
 
         case w @ While( e, t ) ⇒
         {
-          eval( ρ, l_w, e )
-          eval( ρ, l_w, t )
+          var l1 = eval( ρ, l_w, e )
+          var l2 = eval( ρ, l1, t )
           l_w
         }
 
         case Output( e, l ) ⇒
         {
-          println("Output " + l_w + " ⊑ " + l)
-					setOfSets.addToListBuffer(l_w, l)
-					//BFS.algo(l_w)
+          //println("Output " + l_w + " ⊑ " + l)
+		    setOfSets.addToListBuffer(l_w, l)
 
           var l1 = eval( ρ, l_w, e )
-          println("Output " + l1 + " ⊑ " + l)
-					setOfSets.addToListBuffer(l1, l)
-					//BFS.algo(l1)
+          //println("Output " + l1 + " ⊑ " + l)
+			setOfSets.addToListBuffer(l1, l)
 
           l_w
         }
@@ -119,12 +105,12 @@ case class evalLevel(){
 
         case Var( x ) ⇒
            var L = LVar()
-					 setOfSets.addToBigSet(L)
+			setOfSets.addToBigSet(L)
            var l1 = ρ( x )
-           println("Var " + l_w + " ⊑ " + L)
-						setOfSets.addToListBuffer(l_w, L)
-           println("Var " + l1 + " ⊑ " + L)
-						setOfSets.addToListBuffer(l1, L)
+           //println("Var " + l_w + " ⊑ " + L)
+			setOfSets.addToListBuffer(l_w, L)
+           //println("Var " + l1 + " ⊑ " + L)
+			setOfSets.addToListBuffer(l1, L)
           L
 
         case UnOp( op, e ) ⇒
@@ -137,11 +123,11 @@ case class evalLevel(){
           val l1 = eval(ρ, l_w, e1 )
           val l2 = eval( ρ, l_w, e2 )
           var L = LVar()
-					setOfSets.addToBigSet(L)
-          println("BinOp " + l1 + " ⊑ " + L)
-					setOfSets.addToListBuffer(l1, L)
-          println("BinOP " + l2 + " ⊑ " + L)
-					setOfSets.addToListBuffer(l2, L)
+		    setOfSets.addToBigSet(L)
+          //println("BinOp " + l1 + " ⊑ " + L)
+			setOfSets.addToListBuffer(l1, L)
+          //println("BinOP " + l2 + " ⊑ " + L)
+			setOfSets.addToListBuffer(l2, L)
 
           L
         }
@@ -153,22 +139,22 @@ case class evalLevel(){
           val l2 = eval( ρ, l1, t1 )
           val l3 = eval( ρ, l1, t2 )
           var L = LVar()
-					setOfSets.addToBigSet(L)
-          println("If " + l2 + " ⊑ " + L)
-					setOfSets.addToListBuffer(l2, L)
-          println("If " + l3 + " ⊑ " + L)
-					setOfSets.addToListBuffer(l3, L)
+			setOfSets.addToBigSet(L)
+          //println("If " + l2 + " ⊑ " + L)
+			setOfSets.addToListBuffer(l2, L)
+          //println("If " + l3 + " ⊑ " + L)
+			setOfSets.addToListBuffer(l3, L)
 
           L
         }
 
         case In( typ, l ) ⇒ {
             var L = LVar()
-					  setOfSets.addToBigSet(L)
-            println("In " + l_w + " ⊑ " + L)
-						setOfSets.addToListBuffer(l_w, L)
-            println("In " + l + " ⊑ " + L)
-						setOfSets.addToListBuffer(l, L)
+			setOfSets.addToBigSet(L)
+            //println("In " + l_w + " ⊑ " + L)
+			setOfSets.addToListBuffer(l_w, L)
+            //println("In " + l + " ⊑ " + L)
+			setOfSets.addToListBuffer(l, L)
           L
         }
 
@@ -176,15 +162,13 @@ case class evalLevel(){
         // Assign variables to Li
         case Let( xes, t ) ⇒
         {
-            println("Whats in xes? " + xes)
+            //println("Whats in xes? " + xes)
           val xls = xes map { case _ ⇒ LVar()}
-          println("Whats in xls? " + xls)
-
-
+          //println("Whats in xls? " + xls)
 
           val ρ1 = ρ +++ (xes zip xls)
-           println("Whats in ρ1? " + ρ1)
-           xls.foreach(i => { println("Let " + l_w + " ⊑ " + i) 
+          // println("Whats in ρ1? " + ρ1)
+           xls.foreach(i => { //println("Let " + l_w + " ⊑ " + i)
 																setOfSets.addToBigSet(i)
 																setOfSets.addToListBuffer(l_w, i)})
 
--- levels.scala
+++ levels.scala
@@ -5,94 +5,14 @@ import scala.collection.mutable.{ Queue, Set, ListBuffer }
 object illtyped extends Exception
 
 // levels
-sealed abstract class Level {
-  // level completeness
-   /*def ⊑( l:Level ): Unit = (this.find, l.find) match {
-    //TODO
-   case (τ1, τ2) if τ1 == τ2 ⇒ ()
-    case (tv:TVar, τ2) if !(τ2.tvars contains tv) ⇒ tv ▷ τ2
-    case (τ1, tv:TVar) if !(τ1.tvars contains tv) ⇒ tv ▷ τ1
+sealed abstract class Level {}
 
-    case _ ⇒ throw illtyped
-  }  */
+case object Bob extends Level {}
+case object Alice extends Level {}
+case object Public extends Level {}
+case object Secret extends Level {}
 
- /* def ⊑( l:Level ) : Boolean = {
-    true
-
-  }  */
-
-  // return the set of type variable in this type
- /* def tvars: Set[TVar] = this match {
-    case NumT() | BoolT() | StrT() | UnitT() ⇒ Set()
-    case tv:TVar ⇒ Set( tv )
-    case ListT(τ) ⇒ τ.tvars
-    case FunT(τs, τ) ⇒ τs.foldLeft( τ.tvars )( (vs, τ) ⇒ vs ++ τ.tvars )
-  }
-
-  // union-find data structure: find a type's set representative (uses
-  // path compression)
-  def find: Type = this match {
-    case tv:TVar ⇒ tv.parent match {
-      case None ⇒ tv
-      case Some(τ) ⇒ { tv.parent = Some( τ.find ); tv.parent.get }
-    }
-    case ListT(τ) ⇒ ListT( τ.find )
-    case FunT(τs, τ) ⇒ FunT( for (τ ← τs) yield τ.find, τ.find )
-    case _ ⇒ this
-  }     */
-
-  // common to all types
- /* def T = BoolT()
-  def ≈ ( l:Level ) = BoolT()
-  def ≠ ( l:Level ) = BoolT()
-  def ∧ ( l:Level ) = BoolT()
-  def ∨ ( l:Level ) = BoolT()
-  def not = BoolT()
-
-  // type specific
-  def  + ( l:Level ): Level
-  def  − ( l:Level ): Level
-  def  × ( l:Level ): Level
-  def  ÷ ( l:Level ): Level
-  def  ≤ ( l:Level ): Level
-  def  < ( l:Level ): Level
-  def :: ( l:Level ): Level
-  def neg: Level    */
-}
-
-case object Bob extends Level {
-
-}
-
-case object Alice extends Level {
-
-}
-
-case object Public extends Level {
-
- }
-
-case object Secret extends Level {
-
-}
-
-
- //TODO: FIX THIS
-case class L() extends Level {
-/*
-   def  + ( l:Level ) = { l }
-   def  − ( l:Level ) = { l }
-   def  × ( l:Level ) = { l }
-   def  ÷ ( l:Level ) = { l }
-   def  ≤ ( l:Level ) = { l }
-   def  < ( l:Level ) = { l }
-   def :: ( l:Level ) = throw illtyped
-   def neg = Public()   */
- }
-
-case class LVar(x:String) extends Level {
-
-}
+case class LVar(x:String) extends Level {}
 object LVar {
   var id = 0;
 
@@ -102,118 +22,6 @@ object LVar {
     new LVar("L" + id)
   }
 }
-
- /*
-case class NumT() extends Type {
-  def  + ( l:Level ) = { τ ≡ NumT() ; NumT() }
-  def  − ( l:Level ) = { τ ≡ NumT() ; NumT() }
-  def  × ( l:Level ) = { τ ≡ NumT() ; NumT() }
-  def  ÷ ( l:Level ) = { τ ≡ NumT() ; NumT() }
-  def  ≤ ( l:Level ) = { τ ≡ NumT() ; BoolT() }
-  def  < ( l:Level ) = { τ ≡ NumT() ; BoolT() }
-  def :: ( l:Level ) = throw illtyped
-  def neg = NumT()
-}
-
-case class BoolT() extends Type {
-  def  + ( l:Level ) = throw illtyped
-  def  − ( l:Level ) = throw illtyped
-  def  × ( l:Level ) = throw illtyped
-  def  ÷ ( l:Level ) = throw illtyped
-  def  ≤ ( l:Level ) = throw illtyped
-  def  < ( l:Level ) = throw illtyped
-  def :: ( l:Level ) = throw illtyped
-  def neg = throw illtyped
-}
-
-case class StrT() extends Type {
-  def  + ( l:Level ) = { τ ≡ StrT() ; StrT() }
-  def  − ( l:Level ) = throw illtyped
-  def  × ( l:Level ) = throw illtyped
-  def  ÷ ( l:Level ) = throw illtyped
-  def  ≤ ( l:Level ) = { τ ≡ StrT() ; BoolT() }
-  def  < ( l:Level ) = { τ ≡ StrT() ; BoolT() }
-  def :: ( l:Level ) = throw illtyped
-  def neg = throw illtyped
-}
-
-case class UnitT() extends Type {
-  def  + ( l:Level ) = throw illtyped
-  def  − ( l:Level ) = throw illtyped
-  def  × ( l:Level ) = throw illtyped
-  def  ÷ ( l:Level ) = throw illtyped
-  def  ≤ ( l:Level ) = throw illtyped
-  def  < ( l:Level ) = throw illtyped
-  def :: ( l:Level ) = throw illtyped
-  def neg = throw illtyped
-}
-
-case class FunT(τs:Seq[Type], l:Level) extends Type {
-  def  + ( τ1:Type ) = throw illtyped
-  def  − ( τ1:Type ) = throw illtyped
-  def  × ( τ1:Type ) = throw illtyped
-  def  ÷ ( τ1:Type ) = throw illtyped
-  def  ≤ ( τ1:Type ) = throw illtyped
-  def  < ( τ1:Type ) = throw illtyped
-  def :: ( τ1:Type ) = throw illtyped
-  def neg = throw illtyped
-}
-
-case class ListT(l:Level) extends Type {
-  def  + ( τ1:Type ) = throw illtyped
-  def  − ( τ1:Type ) = throw illtyped
-  def  × ( τ1:Type ) = throw illtyped
-  def  ÷ ( τ1:Type ) = throw illtyped
-  def  ≤ ( τ1:Type ) = throw illtyped
-  def  < ( τ1:Type ) = throw illtyped
-  def :: ( τ1:Type ) = { τ1 ≡ τ ; ListT(τ) }
-  def neg = throw illtyped
-}
-
-// type variables. ann is a flag indicating whether this TVar is
-// annotated (true) or not (false); it's made mutable in order to
-// transfer annotations when TVars are unioned together
-case class TVar(x:String, var ann:Boolean) extends Type {
-  def  + ( τ1:Type ) = { ann = true ; this ≡ τ1 ; τ1 }
-  def  − ( τ1:Type ) = { this ≡ τ1 ; τ1 ≡ NumT() ; NumT() }
-  def  × ( τ1:Type ) = { this ≡ τ1 ; τ1 ≡ NumT() ; NumT() }
-  def  ÷ ( τ1:Type ) = { this ≡ τ1 ; τ1 ≡ NumT() ; NumT() }
-  def  ≤ ( τ1:Type ) = { ann = true ; this ≡ τ1 ; τ1 }
-  def  < ( τ1:Type ) = { ann = true ; this ≡ τ1 ; τ1 }
-  def :: ( τ1:Type ) = { this ≡ ListT(τ1) ; this }
-  def neg = { this ≡ NumT() ; NumT() }
-
-  // union-find data structure: union this TVar with some other type
-  // (the other type will always be the new root)
-  def ▷(l:Level) {
-    // check/transfer type annotation
-    if ( ann ) τ match {
-      case tv:TVar ⇒ tv.ann = true
-      case NumT() | StrT() | _:TVar ⇒ ()
-      case _ ⇒ throw illtyped
-    }
-
-    // merge equivalence classes
-    this.parent = Some( τ )
-  }
-
-  // union-find data structure: parent pointer (None if this is a root)
-  var parent:Option[Type] = None
-}
-
-// TVar companion object for factory method (generates fresh type variables)
-object TVar {
-  var id = 0;
-
-  def apply(ann:Boolean = false): TVar = 
-  { 
-    id += 1
-    new TVar("T" + id, ann) 
-  }
-}
-
-
-*/
 
 // element 0 in each subset in bigSet denotes the level the set belongs to
 object setOfSets {
@@ -230,7 +38,6 @@ object setOfSets {
 			bigSet.foreach( i => if (i.head == l) {
 						i += levelToAdd
 					}
-
   			)	 
 	}
 
@@ -261,7 +68,7 @@ object BFS {
 					}
 				}
 				else if (rootNode == Alice) {
-					if(head == Public) {
+					if(head == Public || head == Bob) {
 						println("Not Secure")
 						System.exit(1) 
 					}
@@ -279,16 +86,6 @@ object BFS {
 
   			)
   			 queue = queue.distinct.toQueue
-  			 //println(queue)
-  			 //println(visited)
-			}			
-
-				//  if it's still secure, enqueue all its childer
-				// if it's not, stop program
-	
-			// if queue is empty, entire graph has been searched --> program is secure
-              visited.clear
-			// if queue not empty, dequeue node again (repeat step 2)
-			true
+			}
 	}
 }
